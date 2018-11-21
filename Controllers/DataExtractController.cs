bc15869a8ad15999eb26b8e5a1c5a9797d56ddbd
José Antonio De La Fuente Brando <delafuente.brando@gmail.com>
2018-11-21 22:24:25 +0100

    Check License Plate
    Check License Plate

---
+++ Controllers/DataExtractController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoCoMad.Models;
+using MoCoMad.NSoupDataExtract;
+using NSoup;
+using NSoup.Nodes;
+
+namespace MoCoMad.Controllers
+{
+    public class DataExtractController : Controller
+    {
+        // GET: DataExtract
+        public ActionResult Index()
+        {
+            //string _url = "https://www.google.es";
+            //NsoupHelper _helper = new NsoupHelper();
+            //HtmlData _data = new HtmlData();
+
+            //Document _doc = _helper.UrlConnection(_url);
+            //_data = _helper.FillHtmlData(_doc, _licPlate);
+
+            return View();
+        }
+
+        //// GET: DataExtract/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: DataExtract/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: DataExtract/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: DataExtract/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: DataExtract/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: DataExtract/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: DataExtract/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+
+
+
+    }
+}
--- obj/Debug/netcoreapp2.1/Razor/Views/Shared/_CookieConsentPartial.g.cshtml.cs
+++ obj/Debug/netcoreapp2.1/Razor/Views/Shared/_CookieConsentPartial.g.cshtml.cs
@@ -97,7 +97,7 @@ using Microsoft.AspNetCore.Http.Features;
                     ");
             EndContext();
             BeginContext(1234, 92, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "79603fbdf428436781638d2970199fad", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "c74c4db4a4df40e09082518b96a7c12b", async() => {
                 BeginContext(1312, 10, true);
                 WriteLiteral("Learn More");
                 EndContext();
