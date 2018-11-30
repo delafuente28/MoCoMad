759f266ae84abe2198093092c6df212a147e1c94
José Antonio De La Fuente Brando <delafuente.brando@gmail.com>
2018-11-30 12:24:38 +0100

    Air Quality Map
    Air Quality Map

---
+++ Models/Enums/eMagnitude.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoCoMad.Models.Enums
+{
+    public enum EMagnitude
+    {
+        DIOXIDO_DE_AZUFRE = 01,
+        MONOXIDO_DE_CARBONO = 06,
+        MONOXIDO_DE_NITROGRENO = 07,
+        DIOXIDO_DE_NITROGENO = 08,
+        PARTICULAS_25 = 09,
+        PARTICULAS_10 = 10,
+        OXIDOS_DE_NITROGENO = 12,
+        OZONO = 14,
+        TOLUENO = 20,
+        BENCENO = 30,
+        ETILBENCENO = 35,
+        METAXILENO = 37,
+        PARALIXENO = 38,
+        ORTOXILENO = 39,
+        HEXANO = 42,
+        METANO = 43,
+        HEXANO_NO = 44
+
+
+    }
+}
